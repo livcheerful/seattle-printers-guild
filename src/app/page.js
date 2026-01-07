3d89c0806aa9a7c3285c697a246b072790573bf6
livcheerful <li.v.cheerful@gmail.com>
2026-01-06 23:29:00 -0800

    more mobile fixes

--- src/app/page.js
+++ src/app/page.js
@@ -14,14 +14,16 @@ export default async function Home() {
       style={{ backgroundImage: `url(/grid.png)` }}
     >
       <div className="md:absolute w-full h-fit top-1/6 left-0 flex flex-col gap-3 items-center justify-center">
-        <img className="w-60" src="/self.png" />
-        <h1 className="font-spg text-black text-[4rem] font-bold">
+        <img className=" w-1/2 md:w-60" src="/self.png" />
+        <h1 className="font-spg text-center md:text-left text-black text-4xl md:text-[4rem] font-bold">
           Seattle Printers' Guild
         </h1>
       </div>
       <Link href="/schedule">
         <StickyNote className=" md:absolute -rotate-6 text-black top-8 left-12 bg-spg-yellow p-4 font-spg  text-2xl w-64 ">
-          <div className="text-center font-black text-4xl">Upcoming events</div>
+          <div className="text-center font-black text-3xl md:text-4xl underline">
+            Upcoming events{" "}
+          </div>
           <ul className="list-disc list-inside font-body font-bold text-sm">
             {events &&
               events.map((event, i) => {
@@ -36,7 +38,7 @@ export default async function Home() {
         </StickyNote>
       </Link>
 
-      <div className="md:absolute right-4 top-8 w-1/3 h-1/2">
+      <div className="hidden md:block md:absolute right-4 top-8 w-1/3 h-1/2">
         <ScrapbookText text={"Upcoming event:"} />
         <div>
           <ScrapbookText
@@ -59,11 +61,11 @@ export default async function Home() {
             scrolling="no"
           ></iframe>
         </StickyNote>
-        <StickyNote className="grow-0 aspect-square -rotate-2 text-center font-bold p-2 font-spg text-2xl  bg-spg-blue w-64 text-white">
+        <StickyNote className="grow-0 aspect-square flex flex-col justify-center -rotate-2 text-center font-bold p-2 font-spg text-2xl  bg-spg-blue w-64 text-white">
           The Seattle Printers' Guild provides free workshops in the King County
           area.
         </StickyNote>
-        <StickyNote className="grow-0 aspect-square rotate-6 text-white font-bold text-center p-2 font-spg text-2xl bg-spg-green w-64 ">
+        <StickyNote className="grow-0 aspect-square flex flex-col justify-center rotate-6 text-white font-bold text-center p-2 font-spg text-2xl bg-spg-green w-64 ">
           <div>
             If you want to support the Guild, donate to @shannon-kao on Venmo
             with the memo line "SPG"
